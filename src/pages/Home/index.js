c76c4a7ad78323f19d1ec9bb4aeea5f2e82a0125
Mvcampbell3 <43685395+Mvcampbell3@users.noreply.github.com>
2021-09-24 14:16:46 -0400

    home update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Home = (props) => {
-    return ( 
+    return (
         <div className="container home-container">
             <h1 className="title">Home</h1>
+            <p className="decr">
+                Head to the Grinch page so see Helmet dynamically adding script
+                tags
+            </p>
+            <p className="decr">
+                Sorry, the last time I worked on this it was almost Christmas
+            </p>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Home;
